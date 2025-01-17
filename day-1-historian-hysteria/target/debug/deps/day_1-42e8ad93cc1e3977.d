4be126f91f714dc3949a0b9531ee2d43b8c2b0ba
Swyzsh <swyzsh@protonmail.com>
2025-01-18 00:45:41 +0530

    init

---
+++ day-1-historian-hysteria/target/debug/deps/day_1-42e8ad93cc1e3977.d
@@ -0,0 +1,5 @@
+/Users/suyash/Swyzsh/Advent-of-Code-2024/day-1-historian-hysteria/target/debug/deps/libday_1-42e8ad93cc1e3977.rmeta: src/main.rs
+
+/Users/suyash/Swyzsh/Advent-of-Code-2024/day-1-historian-hysteria/target/debug/deps/day_1-42e8ad93cc1e3977.d: src/main.rs
+
+src/main.rs:
